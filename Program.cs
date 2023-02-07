c9543f7a63c037b9d817b75f00841e2a0b407d27
JoshuaVarela1 <122906871+JoshuaVarela1@users.noreply.github.com>
2023-02-06 18:49:23 -0600

    Prime Number Checker

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace PrimeNumbers;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int breakWhile = 0;
+        Console.WriteLine("-1 to end program");
+
+        while(breakWhile != -1)
+        {
+            Console.WriteLine("Input an integer");
+
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            breakWhile = input;
+
+            if(breakWhile == -1)
+            {
+                break;
+            }
+
+            int result = -9;
+
+            for (int i = 2; i < input; i++) 
+            {
+
+               result = input % i;
+
+                if(result == 0)
+                {
+                    break;
+                }
+            }
+
+            if(result == 0) {
+                Console.WriteLine(input + " is non-prime");
+            } else {
+                Console.WriteLine(input + " is prime");
+            }
+        }
+    }
+}
